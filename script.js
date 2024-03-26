da6d45eb53318f407f25213c5350500d6aef3b10
jeeten848 <jeetenmandal848@gmail.com>
2024-03-26 22:54:05 +0545

    slider buttons

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 // let size = document.querySelector(".jeeten");
 // size.innerHTML= document.querySelector("body").clientWidth;
+
+let leftSlider = document.getElementById("leftSlider");
+let rightSlider = document.getElementById("rightSlider");
+let timelineBox = document.querySelector(".timelineBox");
+
+let slide = 0;
+let unit;
+
+leftSlider.addEventListener("click",()=>{
+    unit = (slide-1)*90;
+    if(unit>=0){
+        timelineBox.style.transform = `translateX(-${unit}%)`;
+        slide--;
+    }
+    console.log(slide)
+})
+rightSlider.addEventListener("click",()=>{
+    unit = (slide+1)*90;
+    // if slide < 6 then 7 slides are shown 
+    if(slide<6){ 
+        timelineBox.style.transform = `translateX(-${unit}%)`;
+        slide++;
+    }
+    console.log(slide);
+})
